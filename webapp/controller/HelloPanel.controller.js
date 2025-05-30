0b257042cd300aac754f11d490b17c228e591dad
CihanNscc <alcincihan@gmail.com>
2025-05-30 20:41:22 -0300

    Refactor App.controller and update App.view to use XMLView for HelloPanel; add dialogCloseButtonText to i18n properties

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+      onButtonPressed: function () {
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
+      },
+      onOpenDialog: function () {
+        var oView = this.getView();
+
+        if (!oView.byId("helloDialog")) {
+          Fragment.load({
+            id: oView.getId(),
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+            controller: this,
+          }).then(function (oDialog) {
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
+        } else {
+          oView.byId("helloDialog").open();
+        }
+      },
+      onCloseDialog: function () {
+        this.getView().byId("helloDialog").close();
+      },
+    });
+  }
+);
