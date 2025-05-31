22a4c69208b31663cfa62efb019c2df5fb684fce
CihanNscc <alcincihan@gmail.com>
2025-05-31 16:01:37 -0300

    Add navigation functionality to Detail view with back button support and update view to show navigation button.

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent"],
-  function (Controller, UIComponent) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History",
+  ],
+  function (Controller, UIComponent, History) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
@@ -19,6 +23,17 @@ sap.ui.define(
             ),
           model: "invoices",
         });
+      },
+      onNavBack: function (oEvent) {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+          oRouter.navTo("overview", {}, true);
+        }
       },
     });
   }
