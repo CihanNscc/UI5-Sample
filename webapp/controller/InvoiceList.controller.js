5d69afb69e31891c77ecce2e53f2c23683b677e4
CihanNscc <alcincihan@gmail.com>
2025-05-31 15:57:07 -0300

    Update routing for detail view to include invoice path parameter, enhance detail page header with ShipperName and ProductName bindings.

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -30,8 +30,13 @@ sap.ui.define(
         oBinding.filter(aFilter);
       },
       onPress: function (oEvent) {
+        var oItem = oEvent.getSource();
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        oRouter.navTo("detail");
+        oRouter.navTo("detail", {
+          invoicePath: window.encodeURIComponent(
+            oItem.getBindingContext("invoices").getPath().substr(1)
+          ),
+        });
       },
     });
   }
